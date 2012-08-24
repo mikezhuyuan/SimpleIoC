f186a0f1b57f27a331286e46ad7d5f347a61feda
mike.zhu <mike.zhu@ef.com>
2012-08-24 12:29:22 +0800

    first commit

---
+++ IoC.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace SimpleIoC
+{
+    public static class IoC
+    {
+        /// <summary>
+        /// Register an instance
+        /// </summary>
+        public static void Register<T>(T instance) where T : class
+        {
+            _loaders[typeof(T)] = () => instance;
+        }
+
+        /// <summary>
+        /// Register a callback to create instance
+        /// </summary>
+        public static void Register<T>(Func<T> loader) where T : class
+        {
+            _loaders[typeof(T)] = loader;
+        }
+
+        /// <summary>
+        /// Resolve an interface
+        /// </summary>
+        public static T Resolve<T>() where T : class
+        {
+            return (T)Resolve(typeof(T));
+        }
+
+        /// <summary>
+        /// Create an concrete class, only call ctor with all interface args, recursively resolve args
+        /// </summary>        
+        public static T Create<T>() where T : class
+        {
+            var type = typeof(T);
+
+            if (!type.IsClass)
+                throw new ArgumentException(type + " is not class");
+
+            if (type.IsAbstract)
+                throw new ArgumentException(type + " is abstract");
+
+            Func<object> creator = null;
+
+            if (!_creators.TryGetValue(type, out creator))
+            {
+                _creators[type] = creator = GetCreator(type, creator);
+            }
+
+            return (T)creator();
+        }
+
+        /// <summary>
+        /// Clear all registries
+        /// </summary>
+        public static void ClearAll()
+        {
+            _loaders.Clear();
+        }
+
+        #region Non-Public
+        private readonly static Dictionary<Type, Func<object>> _loaders = new Dictionary<Type, Func<object>>();
+        private readonly static Dictionary<Type, Func<object>> _creators = new Dictionary<Type, Func<object>>();
+
+        private static object Resolve(Type type)
+        {
+            if (!type.IsInterface)
+                throw new ArgumentException(type + " is not interface");
+
+            Func<object> loader;
+
+            if (_loaders.TryGetValue(type, out loader))
+            {
+                return loader();
+            }
+
+            throw new ArgumentException("Unable to resolve type " + type);
+        }
+
+        private static Func<object> GetCreator(Type type, Func<object> creator)
+        {
+            var ctors = type.GetConstructors();
+            foreach (var ctor in ctors)
+            {
+                bool allInterfaces = true;
+                foreach (var arg in ctor.GetParameters())
+                {
+                    if (!arg.ParameterType.IsInterface)
+                    {
+                        allInterfaces = false;
+                        break;
+                    }
+                }
+
+                if (allInterfaces)
+                {
+                    var method = new DynamicMethod("Create" + type.Name, type, Type.EmptyTypes, typeof(IoC).Module);
+                    var il = method.GetILGenerator();
+                    var resolve = typeof(IoC).GetMethod("Resolve", Type.EmptyTypes);
+
+                    var argVals = new List<object>();
+                    foreach (var arg in ctor.GetParameters())
+                    {
+                        il.Emit(OpCodes.Call, resolve.MakeGenericMethod(arg.ParameterType));
+                    }
+
+                    il.Emit(OpCodes.Newobj, ctor);
+                    il.Emit(OpCodes.Ret);
+
+                    creator = (Func<object>)method.CreateDelegate(typeof(Func<object>));
+                }
+            }
+
+            if (creator == null)
+                throw new ArgumentException("Unable to create type " + type);
+
+            return creator;
+        }
+
+        #endregion
+    }
+}
---
+++ README.txt
@@ -0,0 +1,50 @@
+A very simple and fast IoC for .net
+
+Example:
+
+static void Main(string[] args)
+{
+    var ex = new Exception();
+    IoC.Register<INested3>(() => new Nested3());
+    IoC.Register<INested2>(() => IoC.Create<Nested2>());
+    IoC.Register<INested1>(() => IoC.Create<Nested1>());
+
+    IoC.Resolve<INested1>();
+    Console.ReadKey();
+}
+
+interface INested3
+{
+}
+
+class Nested3 : INested3
+{
+    public Nested3()
+    {
+        Console.WriteLine("Nested3");
+    }
+}
+
+interface INested2
+{
+}
+
+class Nested2 : INested2
+{
+    public Nested2(INested3 n3)
+    {
+        Console.WriteLine("Nested2");
+    }
+}
+
+interface INested1
+{
+}
+
+class Nested1 : INested1
+{
+    public Nested1(INested2 n2)
+    {
+        Console.WriteLine("Nested1");
+    }
+}
